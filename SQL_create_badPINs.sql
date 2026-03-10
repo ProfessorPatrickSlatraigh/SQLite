559c600593b89238d993cd674ecf61705202a90f
Professor Patrick <89508305+ProfessorPatrickSlatraigh@users.noreply.github.com>
2026-03-10 11:38:14 -0400

    Update SQL_create_badPINs.sql
    Adjusted comments and scaffolding for MySQL

--- SQL_create_badPINs.sql
+++ SQL_create_badPINs.sql
@@ -1,16 +1,17 @@
--- comment out the CREATE statement in line 3 if using DB Browser
--- remove the comment from the next line if not using DB Browser
-CREATE DATABASE IF NOT EXISTS bad_pins;
+-- remove the comment on CREATE statement in line 3 if using MySQL
+-- comment the next line if not using MySQL
+-- CREATE DATABASE IF NOT EXISTS bad_pins;
 
--- comment out the USE statement in line 7 if using DB Browser
--- remove the comment from the next line if not using DB Browser
-USE bad_pins;
+-- remove the comment on the USE statement in line 7 if using MySQL
+-- comment he next line if not using MySQL
+-- USE bad_pins;
 
 CREATE TABLE "COMMON_PINS" (
 	"NUMBER"	INTEGER NOT NULL UNIQUE,
 	"PIN_VALUE"	INTEGER NOT NULL UNIQUE,
 	PRIMARY KEY("NUMBER" AUTOINCREMENT)
 );
+
 
 
 
