d019582c1198e5009921d41263295ef75405e1e1
Professor Patrick <89508305+ProfessorPatrickSlatraigh@users.noreply.github.com>
2026-03-10 10:12:53 -0400

    Update SQL_create_badPINs.sql
    Revised comments for use without DB Browser

--- SQL_create_badPINs.sql
+++ SQL_create_badPINs.sql
@@ -1,12 +1,14 @@
+-- comment out the CREATE statement in line 3 if using DB Browser
+-- remove the comment from the next line if not using DB Browser
+CREATE DATABASE IF NOT EXISTS bad_pins;
 
--- remove the comment from the next line if not using DBBrowser
--- CREATE DATABASE IF NOT EXISTS bad_pins;
-
--- remove the comment from the next line if not using DBBrowser
--- USE bad_pins;
+-- comment out the USE statement in line 7 if using DB Browser
+-- remove the comment from the next line if not using DB Browser
+USE bad_pins;
 
 CREATE TABLE "COMMON_PINS" (
 	"NUMBER"	INTEGER NOT NULL UNIQUE,
 	"PIN_VALUE"	INTEGER NOT NULL UNIQUE,
 	PRIMARY KEY("NUMBER" AUTOINCREMENT)
 );
+
