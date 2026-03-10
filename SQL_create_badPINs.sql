a3782d286e9a0cec8860975921ef975cb557a258
Professor Patrick <89508305+ProfessorPatrickSlatraigh@users.noreply.github.com>
2026-03-10 10:52:23 -0400

    Update SQL_create_badPINs.sql
    Tweaked SQL USE statement to comment

--- SQL_create_badPINs.sql
+++ SQL_create_badPINs.sql
@@ -4,11 +4,12 @@ CREATE DATABASE IF NOT EXISTS bad_pins;
 
 -- comment out the USE statement in line 7 if using DB Browser
 -- remove the comment from the next line if not using DB Browser
-USE bad_pins;
+-- USE bad_pins;
 
 CREATE TABLE "COMMON_PINS" (
 	"NUMBER"	INTEGER NOT NULL UNIQUE,
 	"PIN_VALUE"	INTEGER NOT NULL UNIQUE,
 	PRIMARY KEY("NUMBER" AUTOINCREMENT)
 );
+
 
